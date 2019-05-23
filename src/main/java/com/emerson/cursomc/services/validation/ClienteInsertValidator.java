f375990246cce4acb734c0f8ce0d701306402a93
Coutinho <emerson.s.coutinho@accenture.com>
2019-05-23 16:00:30 -0300

    Valicadao customizada: CPF ou CNPJ part 1

---
+++ src/main/java/com/emerson/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,31 @@
+package com.emerson.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.emerson.cursomc.dto.ClienteNewDTO;
+import com.emerson.cursomc.resources.exception.FieldMessage;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+
+		List<FieldMessage> list = new ArrayList<>();
+		
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
